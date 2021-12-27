fdbec24093a8e8ad2b4a1cb06ac94a6a5186c715
Kevin Nolasco <kevnolasco95@gmail.com>
2021-12-27 14:58:54 -0800

    adding and applying fisher

--- DiscriminativeClassifiers/myClassifiers.py
+++ DiscriminativeClassifiers/myClassifiers.py
@@ -123,7 +123,7 @@ class Perceptron:
         self.X = X
         self.N, self.D = self.X.shape
         self.y = y
-        self.n_iter = n_iter
+        self.n_iter = int(n_iter)
         self.lr = lr
         self.converged = False
 
@@ -149,3 +149,35 @@ class Perceptron:
         # return values
         self.beta = beta
         self.yhat = to_binary(sign(np.dot(self.X, self.beta)))
+
+# now fisher's discriminant
+
+class FisherLinearDiscriminant:
+
+    def fit(self, X, y):
+        # initialize attributes
+        self.X = X
+        self.y = y
+        self.N, self.D = self.X.shape
+
+        # calculate the class means
+        X0 = X[y == 0]
+        X1 = X[y == 1]
+        mu0 = X0.mean(0)
+        mu1 = X1.mean(0)
+
+        # calculate sigma
+        Sigma_w = np.zeros((self.D, self.D)) # zeros not empty
+        for x0 in X0:
+            x0_minus_mu0 = (x0 - mu0).reshape(-1,1)
+            Sigma_w += np.dot(x0_minus_mu0, x0_minus_mu0.T)
+        
+        for x1 in X1:
+            x1_minus_mu1 = (x1 - mu1).reshape(-1,1)
+            Sigma_w += np.dot(x1_minus_mu1, x1_minus_mu1.T)
+        
+        Sigma_w_inverse = np.linalg.inv(Sigma_w)
+
+        # calculate beta
+        self.beta = np.dot(Sigma_w_inverse, mu1 - mu0)
+        self.f = np.dot(X, self.beta)
