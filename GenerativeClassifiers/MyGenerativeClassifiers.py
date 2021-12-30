11a358fc43b8054d146e4942ea349e8ed09d9ba7
Kevin Nolasco <kevnolasco95@gmail.com>
2021-12-30 15:06:19 -0800

    adding and implementing QDA

--- GenerativeClassifiers/MyGenerativeClassifiers.py
+++ GenerativeClassifiers/MyGenerativeClassifiers.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import numpy as np
+from numpy.lib.arraysetops import unique
 
 class LDA:
     
@@ -18,7 +19,7 @@ class LDA:
         # get the mu for each class
         self.mu_ks = []
         self.Sigma = np.zeros((self.D, self.D))
-        for i, k in enumerate(self.unique_y):
+        for _, k in enumerate(self.unique_y):
             X_k = self.X[self.y == k] # get all X for class k
             mu_k = X_k.mean(0).reshape(self.D,1)
             self.mu_ks.append(mu_k)
@@ -45,10 +46,62 @@ class LDA:
             x_n = x_n.reshape(-1,1)
             p_ks = np.empty(len(self.unique_y))
 
-            for j, k in enumerate(self.unique_y):
+            for j, _ in enumerate(self.unique_y):
                 p_x_given_y = self._mvn_density(x_n, self.mu_ks[j], self.Sigma)
                 p_y_given_x = self.pi_ks[j]*p_x_given_y
                 p_ks[j] = p_y_given_x
         
             y_n[i] = self.unique_y[np.argmax(p_ks)]
+        return y_n
+
+class QDA:
+
+    """
+    QDA is implemented very similarly to LDA, except each observation has it's own Sigma (covariance matrix)
+    """
+
+    def fit(self, X, y):
+
+        # initialize attributes
+        self.X = X
+        self.y = y
+        self.N, self.D = self.X.shape
+
+        # save prior probabilities
+        self.unique_y, unique_y_counts = np.unique(self.y, return_counts = True)
+        self.pi_ks = unique_y_counts / self.N
+
+        # calculate mu and sigma per class
+        self.mu_ks = []
+        self.Sigma_ks = []
+        for i, k in enumerate(self.unique_y):
+            X_k = self.X[self.y == k]
+            mu_k = X_k.mean(0).reshape(self.D, 1)
+            self.mu_ks.append(mu_k)
+
+            Sigma_k = np.zeros((self.D, self.D))
+            for x_n in X_k:
+                x_n = x_n.reshape(-1,1)
+                x_n_minus_mu_k = (x_n - mu_k)
+                Sigma_k += np.dot(x_n_minus_mu_k, x_n_minus_mu_k.T)
+            self.Sigma_ks.append(Sigma_k / len(X_k))
+
+    # methods for the classifications
+    def _mvn_density(self, x_n, mu_k, Sigma_k):
+        x_n_minus_mu_k = (x_n - mu_k)
+        return np.linalg.det(Sigma_k)**(-1/2)*np.exp(-(1/2)*x_n_minus_mu_k.T @ np.linalg.inv(Sigma_k) @ x_n_minus_mu_k)
+
+    def classify(self, X_test):
+        y_n = np.empty(len(X_test))
+        for i, x_n in enumerate(X_test):
+            x_n = x_n.reshape(-1,1)
+            p_ks = np.empty(len(self.unique_y))
+
+            for j, _ in enumerate(self.unique_y):
+                p_x_given_y = self._mvn_density(x_n, self.mu_ks[j], self.Sigma_ks[j])
+                p_y_given_x = self.pi_ks[j]*p_x_given_y
+                p_ks[j] = p_y_given_x
+
+            y_n[i] = self.unique_y[np.argmax(p_ks)]
+
         return y_n
