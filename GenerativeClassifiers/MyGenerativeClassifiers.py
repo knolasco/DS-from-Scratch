d0c07cb177b99651e0e1c976e5d5be276b4ed37f
Kevin Nolasco <kevnolasco95@gmail.com>
2021-12-30 16:31:46 -0800

    adding naivebayes

--- GenerativeClassifiers/MyGenerativeClassifiers.py
+++ GenerativeClassifiers/MyGenerativeClassifiers.py
@@ -74,7 +74,7 @@ class QDA:
         # calculate mu and sigma per class
         self.mu_ks = []
         self.Sigma_ks = []
-        for i, k in enumerate(self.unique_y):
+        for _, k in enumerate(self.unique_y):
             X_k = self.X[self.y == k]
             mu_k = X_k.mean(0).reshape(self.D, 1)
             self.mu_ks.append(mu_k)
@@ -104,4 +104,94 @@ class QDA:
 
             y_n[i] = self.unique_y[np.argmax(p_ks)]
 
+        return y_n
+
+class NaiveBayes:
+
+    def fit(self, X, y, distributions = None):
+
+        # initialize attributes
+        self.X = X
+        self.y = y
+        self.N, self.D = self.X.shape
+
+        if distributions is None:
+            distributions = ['normal' for i in range(len(self.y))]
+        
+        self.distributions = distributions
+
+        # find prior probabilities
+        self.unique_y, unique_y_counts = np.unique(self.y, return_counts = True)
+        self.pi_ks = unique_y_counts / self.N
+
+        # estimate parameters
+        self.parameters = []
+        for _, k in enumerate(self.unique_y):
+            X_k = self.X[self.y == k]
+            self.parameters.append(self._estimate_class_parameters(X_k))
+
+    def _estimate_class_parameters(self, X_k):
+
+        # we fill class_parameters based on the distribution per class
+        class_parameters = []
+
+        for d in range(self.D):
+            X_kd = X_k[:, d] # work through each column
+
+            if self.distributions[d] == 'normal':
+                mu = np.mean(X_kd)
+                sigma2 = np.var(X_kd)
+                class_parameters.append([mu, sigma2])
+
+            if self.distributions[d] == 'bernoulli':
+                p = np.mean(X_kd)
+                class_parameters.append(p)
+            
+            if self.distributions[d] == 'poisson':
+                lam = np.mean(X_kd)
+                class_parameters.append(p)
+        return class_parameters
+    
+
+    # make classifications
+    def _get_class_probabilities(self, x_n, j):
+
+        class_parameters = self.parameters[j]
+        class_probability = 1
+
+        for d in range(self.D):
+            x_nd = x_n[d]
+
+            if self.distributions[d] == 'normal':
+                mu, sigma2 = class_parameters[d]
+                class_probability *= sigma2**(-1/2)*np.exp(-(x_nd - mu)**2/sigma2)
+            
+            if self.distributions[d] == 'bernoulli':
+                p = class_parameters[d]
+                class_probability *= (p**x_nd)*(1-p)**(1-x_nd)
+            
+            if self.distributions == 'poisson':
+                lam = class_parameters[d]
+                class_probability *= np.exp(-lam)*lam**x_nd
+
+        return class_probability
+    
+    def classify(self, X_test):
+
+        y_n = np.empty(len(X_test))
+
+        for i, x_n in enumerate(X_test):
+
+            x_n = x_n.reshape(-1,1)
+            p_ks = np.empty(len(self.unique_y))
+
+            for j, _ in enumerate(self.unique_y):
+
+                p_x_given_y = self._get_class_probabilities(x_n, j)
+                p_y_given_x = self.pi_ks[j]*p_x_given_y
+
+                p_ks[j] = p_y_given_x
+            
+            y_n[i] = self.unique_y[np.argmax(p_ks)]
+        
         return y_n
