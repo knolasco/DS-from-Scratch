1289c67b2135a82688330c0cdfd33610bd68a9f6
Kevin Nolasco <knolasco@ucsb.edu>
2022-01-09 18:01:25 -0800

    adding modified decision tree classifier for ADA

--- TreeEnsembleMethods/MyEnsemble.py
+++ TreeEnsembleMethods/MyEnsemble.py
@@ -95,3 +95,216 @@ def cross_entropy(y, weights):
 
 def split_loss(child1, child2, weights1, weights2, loss = cross_entropy):
     return (len(child1)*loss(child1, weights1) + len(child2)*loss(child2, weights2))/(len(child1) + len(child2))
+
+
+# adding decision tree classifier used for ADA
+# slightly different than classifier in MyDecisionTree.py
+# this classifier keeps track of the ID so that we could use the weights
+
+## Helper Classes
+class Node:
+    
+    def __init__(self, Xsub, ysub, observations, ID, depth = 0, parent_ID = None, leaf = True):
+        self.Xsub = Xsub
+        self.ysub = ysub
+        self.observations = observations
+        self.ID = ID
+        self.size = len(ysub)
+        self.depth = depth
+        self.parent_ID = parent_ID
+        self.leaf = leaf
+        
+
+class Splitter:
+    
+    def __init__(self):
+        self.loss = np.inf
+        self.no_split = True
+        
+    def _replace_split(self, loss, d, dtype = 'quant', t = None, L_values = None):
+        self.loss = loss
+        self.d = d
+        self.dtype = dtype
+        self.t = t
+        self.L_values = L_values  
+        self.no_split = False
+
+        
+## Main Class
+class DecisionTreeClassifier:
+    
+    #############################
+    ######## 1. TRAINING ########
+    #############################
+    
+    ######### FIT ##########
+    def fit(self, X, y, weights, loss_func = cross_entropy, max_depth = 100, min_size = 2, C = None):
+        
+        ## Add data
+        self.X = X
+        self.y = y
+        self.N, self.D = self.X.shape
+        dtypes = [np.array(list(self.X[:,d])).dtype for d in range(self.D)]
+        self.dtypes = ['quant' if (dtype == float or dtype == int) else 'cat' for dtype in dtypes]
+        self.weights = weights
+        
+        ## Add model parameters
+        self.loss_func = loss_func
+        self.max_depth = max_depth
+        self.min_size = min_size
+        self.C = C
+        
+        ## Initialize nodes
+        self.nodes_dict = {}
+        self.current_ID = 0
+        initial_node = Node(Xsub = X, ysub = y, observations = np.arange(self.N), ID = self.current_ID, parent_ID = None)
+        self.nodes_dict[self.current_ID] = initial_node
+        self.current_ID += 1
+        
+        # Build
+        self._build()
+
+    ###### BUILD TREE ######
+    def _build(self):
+        
+        eligible_buds = self.nodes_dict 
+        for layer in range(self.max_depth):
+            
+            ## Find eligible nodes for layer iteration
+            eligible_buds = {ID:node for (ID, node) in self.nodes_dict.items() if 
+                                (node.leaf == True) &
+                                (node.size >= self.min_size) & 
+                                (~ct.all_rows_equal(node.Xsub)) &
+                                (len(np.unique(node.ysub)) > 1)}
+            if len(eligible_buds) == 0:
+                break
+            
+            ## split each eligible parent
+            for ID, bud in eligible_buds.items():
+                                
+                ## Find split
+                self._find_split(bud)
+                
+                ## Make split
+                if not self.splitter.no_split:
+                    self._make_split()
+                
+    ###### FIND SPLIT ######
+    def _find_split(self, bud):
+        
+        ## Instantiate splitter
+        splitter = Splitter()
+        splitter.bud_ID = bud.ID
+        
+        ## For each (eligible) predictor...
+        if self.C is None:
+            eligible_predictors = np.arange(self.D)
+        else:
+            eligible_predictors = np.random.choice(np.arange(self.D), self.C, replace = False)
+        for d in sorted(eligible_predictors):
+            Xsub_d = bud.Xsub[:,d]
+            dtype = self.dtypes[d]
+            if len(np.unique(Xsub_d)) == 1:
+                continue
+
+            ## For each value...
+            if dtype == 'quant':
+                for t in np.unique(Xsub_d)[:-1]:
+                    L_condition = Xsub_d <= t
+                    ysub_L = bud.ysub[L_condition]
+                    ysub_R = bud.ysub[~L_condition]
+                    weights_L = self.weights[bud.observations][L_condition]
+                    weights_R = self.weights[bud.observations][~L_condition]
+                    loss = split_loss(ysub_L, ysub_R,
+                                      weights_L, weights_R,
+                                      loss = self.loss_func)
+                    if loss < splitter.loss:
+                        splitter._replace_split(loss, d, 'quant', t = t)
+            else:
+                for L_values in ct.possible_splits(np.unique(Xsub_d)):
+                    L_condition = np.isin(Xsub_d, L_values)
+                    ysub_L = bud.ysub[L_condition]
+                    ysub_R = bud.ysub[~L_condition]
+                    weights_L = self.weights[bud.observations][L_condition]
+                    weights_R = self.weights[bud.observations][~L_condition]
+                    loss = split_loss(ysub_L, ysub_R,
+                                      weights_L, weights_R,
+                                      loss = self.loss_func)
+                    if loss < splitter.loss: 
+                        splitter._replace_split(loss, d, 'cat', L_values = L_values)
+                        
+        ## Save splitter
+        self.splitter = splitter
+    
+    ###### MAKE SPLIT ######
+    def _make_split(self):
+        
+        ## Update parent node
+        parent_node = self.nodes_dict[self.splitter.bud_ID]
+        parent_node.leaf = False
+        parent_node.child_L = self.current_ID
+        parent_node.child_R = self.current_ID + 1
+        parent_node.d = self.splitter.d
+        parent_node.dtype = self.splitter.dtype
+        parent_node.t = self.splitter.t        
+        parent_node.L_values = self.splitter.L_values
+        
+        ## Get X and y data for children
+        if parent_node.dtype == 'quant':
+            L_condition = parent_node.Xsub[:,parent_node.d] <= parent_node.t
+        else:
+            L_condition = np.isin(parent_node.Xsub[:,parent_node.d], parent_node.L_values)
+        Xchild_L = parent_node.Xsub[L_condition]
+        ychild_L = parent_node.ysub[L_condition]
+        child_observations_L = parent_node.observations[L_condition]
+        Xchild_R = parent_node.Xsub[~L_condition]
+        ychild_R = parent_node.ysub[~L_condition]
+        child_observations_R = parent_node.observations[~L_condition]
+        
+        ## Create child nodes
+        child_node_L = Node(Xchild_L, ychild_L, child_observations_L,
+                            ID = self.current_ID, depth = parent_node.depth + 1,
+                            parent_ID = parent_node.ID)
+        child_node_R = Node(Xchild_R, ychild_R, child_observations_R,
+                            ID = self.current_ID + 1, depth = parent_node.depth + 1,
+                            parent_ID = parent_node.ID)
+        self.nodes_dict[self.current_ID] = child_node_L
+        self.nodes_dict[self.current_ID + 1] = child_node_R
+        self.current_ID += 2
+                
+            
+    #############################
+    ####### 2. PREDICTING #######
+    #############################
+    
+    ###### LEAF MODES ######
+    def _get_leaf_modes(self):
+        self.leaf_modes = {}
+        for node_ID, node in self.nodes_dict.items():
+            if node.leaf:
+                values, counts = np.unique(node.ysub, return_counts=True)
+                self.leaf_modes[node_ID] = values[np.argmax(counts)]
+    
+    ####### PREDICT ########
+    def predict(self, X_test):
+        
+        # Calculate leaf modes
+        self._get_leaf_modes()
+        
+        yhat = []
+        for x in X_test:
+            node = self.nodes_dict[0] 
+            while not node.leaf:
+                if node.dtype == 'quant':
+                    if x[node.d] <= node.t:
+                        node = self.nodes_dict[node.child_L]
+                    else:
+                        node = self.nodes_dict[node.child_R]
+                else:
+                    if x[node.d] in node.L_values:
+                        node = self.nodes_dict[node.child_L]
+                    else:
+                        node = self.nodes_dict[node.child_R]
+            yhat.append(self.leaf_modes[node.ID])
+        return np.array(yhat)
+            
